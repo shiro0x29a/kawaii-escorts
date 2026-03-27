db8c35c1279a391ba51368ddd1374cf8b0cb89e7
anik <anik@anik.anik>
2026-03-27 18:47:41 +0300

    add ui/

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,24 @@
+// UI Components Library
+export { Button } from './Button';
+export type { ButtonProps } from './Button';
+
+export { Input } from './Input';
+export type { InputProps } from './Input';
+
+export { Dropdown } from './Dropdown';
+export type { DropdownOption, DropdownProps } from './Dropdown';
+
+export { Card } from './Card';
+export type { CardProps } from './Card';
+
+export { Pagination } from './Pagination';
+export type { PaginationProps } from './Pagination';
+
+export { Modal } from './Modal';
+export type { ModalProps } from './Modal';
+
+export { SearchBox } from './SearchBox';
+export type { SearchSuggestion, SearchBoxProps } from './SearchBox';
+
+export { Tabs } from './Tabs';
+export type { Tab, TabsProps } from './Tabs';
